0fc58c666bdfdb9a52b68aac5e49990df80df112
Irina <petitpapillon82@gmail.com>
2022-09-04 15:17:19 +0400

    Add Example013...factorial

---
+++ Example013_RecursionAlgorithm_factorial/Program.cs
@@ -0,0 +1,21 @@
+﻿/*int Factorial (int n)
+{
+    //1! = 1
+    //0! = 1
+    if(n == 1) return 1;
+    else return n * Factorial(n - 1);
+}
+
+Console.WriteLine(Factorial(3)); //1*2*3 = 6
+*/
+
+double Factorial (int n)
+{
+    if(n == 1) return 1;
+    else return n * Factorial(n - 1);
+}
+for (int i = 1; i < 40; i++)
+{
+   Console.WriteLine($"[{i} = {Factorial(i)}"); 
+}
+
